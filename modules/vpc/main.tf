356e3ab9f5b249779bda0913c01b6beff267d71e
Shiv <shivanand.meti@gmail.com>
2026-09-13 21:41:00 -0500

    Fix terraform formatting

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -16,21 +16,21 @@ resource "aws_subnet" "public" {
   cidr_block              = var.public_subnet_cidr
   map_public_ip_on_launch = true
   availability_zone       = "${var.aws_region}a"
-  tags = { Name = "${var.project_name}-public-subnet" }
+  tags                    = { Name = "${var.project_name}-public-subnet" }
 }
 
 resource "aws_subnet" "private" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = var.private_subnet_cidr
   availability_zone = "${var.aws_region}b"
-  tags = { Name = "${var.project_name}-private-subnet" }
+  tags              = { Name = "${var.project_name}-private-subnet" }
 }
 
 resource "aws_subnet" "private_2" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = var.private_subnet_2_cidr
   availability_zone = "${var.aws_region}c"
-  tags = { Name = "${var.project_name}-private-subnet-2" }
+  tags              = { Name = "${var.project_name}-private-subnet-2" }
 }
 
 resource "aws_route_table" "public" {
